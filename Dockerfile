f473cafd8efc848d2a0aa9cd1aa80cb43186fa6d
jimdavis66 <james.norman.davis@gmail.com>
2024-06-14 11:29:47 +1000

    Update compose.yml file

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,4 @@
 FROM python:3.9.19-slim-bullseye
 RUN pip install flask ldap3 python-dotenv
 COPY app .
-CMD ["python", "-m", "flask", "run", "--host=0.0.0.0"]
+CMD ["python", "-u", "app.py"]
--- app/app.py
+++ app/app.py
@@ -97,4 +97,4 @@ def failure():
     return "Failed to update the Card ID number. Please check the values and try again."
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5050)
